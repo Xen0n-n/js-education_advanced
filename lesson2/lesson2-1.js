cd0580f174384736f1bae14424b99cb2dba1f7f0
Xen0n_n <nsoft13@gmail.com>
2024-04-20 17:20:42 +0300

    added lesson2 ex1

---
+++ lesson2/lesson2-1.js
@@ -0,0 +1,24 @@
+//ES5
+
+'use strict'
+
+//констуктор по которому будет создан объект класса
+function Person(firstName, lastName, yearOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+
+    this.calcAge = function () {
+        console.log(new Date().getFullYear() - this.yearOfBirth);
+    }
+
+}
+
+let user1 = new Person('Petr', 'Petrov', 1990);
+let user2 = new Person('Ivan', 'Ivanov', 2001);
+
+console.log(user1);
+
+user1.__proto__.test = 10;
+
+console.log(user1);
