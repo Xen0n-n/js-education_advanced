19b59bf94179258bd97e70c7a826a4bc5362d369
Xen0n_n <nsoft13@gmail.com>
2024-04-21 11:47:24 +0300

    added static methods

--- lesson2-ES6/lesson2-1-ES6.js
+++ lesson2-ES6/lesson2-1-ES6.js
@@ -10,6 +10,14 @@ class Person {
         console.log(new Date().getFullYear() - this.yearOfBirth);
     }
 
+    static compareAge(obj1, obj2) {
+        if (obj1.yearOfBirth < obj2.yearOfBirth)
+            return console.log(`${obj1.firstName} старше`)
+        else if (obj1.yearOfBirth > obj2.yearOfBirth)
+            return console.log(`${obj2.firstName} старше`)
+        return console.log('Люди одного года рождения')
+    }
+
     // все статические методы привязаны к классу, а не к объекту, в них запрещено использовать this
     static test(x) {
         if(x === undefined){
@@ -17,10 +25,10 @@ class Person {
         }
         return x * 2;
     }
-
 }
 
-// let user1 = new Person('Ivan', 1990)
-// console.log(user1)
 
-console.log(Person.test(10));
+let user1 = new Person('Ivan', 1990)
+let user2 = new Person('Igor', 1975)
+
+Person.compareAge(user1, user2)
